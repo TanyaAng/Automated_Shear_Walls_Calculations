d3256fcad8b683c9a604f45527ea2385512dd78c
TanyaAng <t.j.angelova@gmail.com>
2022-06-25 23:14:29 +0300

    GUI Application

--- helpers/calculations/calculate_Aa1_Aa2.py
+++ helpers/calculations/calculate_Aa1_Aa2.py
@@ -1,9 +1,9 @@
-from helpers.rebars.rebars_db import REBARS_MAPPER, rebar_area_cm2
+from helpers.rebars.rebars_db import rebar_area_cm2, rebars
 
 
 def calculate_Aa1_Aa2(required_reinforcement, number_of_bars):
     required_rebar_cm2 = required_reinforcement / number_of_bars
-    for key, value in REBARS_MAPPER.items():
-        if required_rebar_cm2<=rebar_area_cm2(key):
-            return key
+    for diameter in rebars():
+        if required_rebar_cm2<=rebar_area_cm2(diameter):
+            return diameter
     return "Change column size!!!"
