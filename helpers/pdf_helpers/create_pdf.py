4382cafeaef3d2820fd2571d2457bd158f68ebec
TanyaAng <t.j.angelova@gmail.com>
2022-06-26 23:02:45 +0300

    GUI application

--- helpers/pdf_helpers/create_pdf.py
+++ helpers/pdf_helpers/create_pdf.py
@@ -10,28 +10,28 @@ def create_PDF(dictionary):
     pdf.add_page()
     pdf.set_font('Arial', size=16)
 
-    pdf.cell(200, 20, txt=f"Shear Walls Max Values Per Level", ln=1, align='C')
+    pdf.cell(200, 20, txt=f"Shear Walls Max Reinforcement Values Per Level", ln=1, align='C')
     previous_wall_num = 0
     previous_storey_level = 0
     for wall_name, values in dictionary.items():
         wall_num, storey_num = wall_name[1:].split('-')
-        storey_num=int(storey_num)
+        storey_num = int(storey_num)
         pdf.set_font('Arial', style='B', size=10)
 
         if previous_wall_num != 0 and previous_wall_num != wall_num:
-            previous_storey_level = int(storey_num)+1
+            previous_storey_level = int(storey_num) + 1
             pdf.cell(0, 6, ln=1)
 
         pdf.set_fill_color(118, 180, 180)
         pdf.cell(15, 6, txt=f'W{wall_num}', border=1, ln=0, align='C', fill=True)
 
-        if previous_storey_level != 0 and storey_num != previous_storey_level - 1:
+        if previous_storey_level == 0 or storey_num == previous_storey_level - 1:
+            pdf.cell(20, 6, txt=f'Storey {storey_num}', border=1, ln=0, align='C')
+            previous_storey_level = storey_num
+        else:
             pdf.set_fill_color(255, 102, 102)
             pdf.cell(20, 6, txt=f'Storey {storey_num}', border=1, ln=0, align='C', fill=True)
             previous_storey_level = 0
-        else:
-            pdf.cell(20, 6, txt=f'Storey {storey_num}', border=1, ln=0, align='C')
-            previous_storey_level = storey_num
 
         for type, value in values.items():
             pdf.set_font('Arial', style='', size=10)
